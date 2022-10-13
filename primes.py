dfd42ac5c8bfe446d82a51866eb7d7960eaade09
Sydney <k2002418@kcl.ac.uk>
2022-10-13 23:51:45 +0100

    main

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [] 
+    i=2
+    while len(list) != number_of_primes:
+        if isPrimes(i):
+            list.append(i)
+            i+=1
+        else:
+            i+=1
+                 
     return list
+    
+def isPrimes(n):
+    for k in range(2,n):
+        if n%k == 0:
+            return False
+    else:
+            return True
+
+
